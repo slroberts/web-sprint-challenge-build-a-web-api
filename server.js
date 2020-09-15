31f4d2050ee33a45ee1e1e280c567bc26d51188c
slroberts <shomariroberts@gmail.comclear>
2020-09-14 22:22:08 -0400

    fixed from undefined in logger function

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ server.get("/", (req, res) => {
 function logger(req, res, next) {
   console.log(
     `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
-      "Origin"
+      "host"
     )}`
   );
 
